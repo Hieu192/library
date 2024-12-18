ee4acdd97da3f6f083e04e20b0f3230c96b931ec
Hieu <21520235@gm.uit.edu.vn>
2024-12-19 00:03:01 +0700

    update home

--- frontend/src/Components/PopularBooks.js
+++ frontend/src/Components/PopularBooks.js
@@ -1,74 +1,55 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./PopularBooks.css";
+import { useDispatch, useSelector } from "react-redux";
+import { FetchPopularBooks } from "../redux/slices/app";
+import { Link } from "@material-ui/core";
 
 function PopularBooks() {
+  const dispatch = useDispatch();
+  const { booksPopular } = useSelector((state) => state.app);
+  console.log("booksRecent::",booksPopular);
+  useEffect(() => {
+      dispatch(FetchPopularBooks());
+  }, []);
   return (
     <div className="popularbooks-container">
       <h className="popularbooks-title">Sách phổ biến </h>
       <div className="popularbooks">
         <div className="popularbook-images">
-          <img
-            className="popular-book"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS34iIDoKVXOhKhdwsiGSLc9RJmtq_lSQDig&usqp=CAU"
-            alt=""
-          ></img>
-          <img
-            className="popular-book"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfRHNwRyPkTxnMOzOvv5dOK4OS_lq4-2Yugg&usqp=CAU"
-            alt=""
-          ></img>
-          <img
-            className="popular-book"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7ElcNy_v2Ri1H3VhYjYP1MzR6zBUwFQWbOirCkaqcfOqJnbrK5ZvdZNUwEfrlmJwn7pA&usqp=CAU"
-            alt=""
-          ></img>
-          <img
-            className="popular-book"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS34iIDoKVXOhKhdwsiGSLc9RJmtq_lSQDig&usqp=CAU"
-            alt=""
-          ></img>
-          <img
-            className="popular-book"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfRHNwRyPkTxnMOzOvv5dOK4OS_lq4-2Yugg&usqp=CAU"
-            alt=""
-          ></img>
-          <img
-            className="popular-book"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7ElcNy_v2Ri1H3VhYjYP1MzR6zBUwFQWbOirCkaqcfOqJnbrK5ZvdZNUwEfrlmJwn7pA&usqp=CAU"
-            alt=""
-          ></img>
-        </div>
-        <div className="popularbook-images">
-          <img
-            className="popular-book"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS34iIDoKVXOhKhdwsiGSLc9RJmtq_lSQDig&usqp=CAU"
-            alt=""
-          ></img>
-          <img
-            className="popular-book"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfRHNwRyPkTxnMOzOvv5dOK4OS_lq4-2Yugg&usqp=CAU"
-            alt=""
-          ></img>
-          <img
-            className="popular-book"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7ElcNy_v2Ri1H3VhYjYP1MzR6zBUwFQWbOirCkaqcfOqJnbrK5ZvdZNUwEfrlmJwn7pA&usqp=CAU"
-            alt=""
-          ></img>
-          <img
-            className="popular-book"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS34iIDoKVXOhKhdwsiGSLc9RJmtq_lSQDig&usqp=CAU"
-            alt=""
-          ></img>
-          <img
-            className="popular-book"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfRHNwRyPkTxnMOzOvv5dOK4OS_lq4-2Yugg&usqp=CAU"
-            alt=""
-          ></img>
-          <img
-            className="popular-book"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7ElcNy_v2Ri1H3VhYjYP1MzR6zBUwFQWbOirCkaqcfOqJnbrK5ZvdZNUwEfrlmJwn7pA&usqp=CAU"
-            alt=""
-          ></img>
+          {booksPopular.map((book) => {
+              return (
+                  <Link to={`/books/${book._id}`}>
+                  <div
+                      key={book._id}
+                      className="book-card"
+                  >
+                      <img src={book.image[0].url} alt=""></img>
+                      <p className="bookcard-title">{book.bookName}</p>
+                      <p className="bookcard-author">{book.author}</p>
+                      <div className="bookcard-category">
+                      <p>{book.categories[0].categoryName}</p>
+                      </div>
+                  </div>
+                  </Link>
+              );
+              })} 
+            {booksPopular.map((book) => {
+              return (
+                  <Link to={`/books/${book._id}`}>
+                  <div
+                      key={book._id}
+                      className="book-card"
+                  >
+                      <img src={book.image[0].url} alt=""></img>
+                      <p className="bookcard-title">{book.bookName}</p>
+                      <p className="bookcard-author">{book.author}</p>
+                      <div className="bookcard-category">
+                      <p>{book.categories[0].categoryName}</p>
+                      </div>
+                  </div>
+                  </Link>
+              );
+              })} 
         </div>
       </div>
     </div>
