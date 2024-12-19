59780a9d63a662880732b21fdde63f16eb0267cb
Hieu <21520235@gm.uit.edu.vn>
2024-12-19 22:00:02 +0700

    update

--- backend/models/model3D/SimpleStructure.js
+++ backend/models/model3D/SimpleStructure.js
@@ -1,19 +1,10 @@
 import mongoose from "mongoose";
 
 const SimpleStructureSchema = new mongoose.Schema({
-    // structureId: {
-    //     type: String,
-    //     require: true,
-    //     unique: true
-    // },
     floorId: { 
         type: mongoose.Types.ObjectId, 
         ref: "Floor" 
     },
-    // faceId: { 
-    //     type: mongoose.Types.ObjectId, 
-    //     ref: "Face" 
-    // },
     faceIds: [{ 
         type: mongoose.Types.ObjectId, 
         ref: "Face" 
--- frontend/src/Components/ReservedBooks.js
+++ frontend/src/Components/ReservedBooks.js
@@ -7,39 +7,54 @@ function ReservedBooks() {
             <h className='reservedbooks-title'>Sách đang chờ về </h>
             <table className='reservedbooks-table'>
                 <tr>
-                    <th>Name</th>
-                    <th>Book</th>
-                    <th>Date</th>
+                    <th>Tác giả </th>
+                    <th>Sách</th>
+                    <th>Ngày </th>
                 </tr>
                 <tr>
-                    <td>Pranav</td>
-                    <td>Rich Dad Poor Dad</td>
-                    <td>12/7/2021</td>
+                    <td> Harper Lee</td>
+                    <td>Giết con chim nhại</td>
+                    <td>12/7/2025 </td>
                 </tr>
                 <tr>
-                    <td>Sashank</td>
-                    <td>The Subtle Art</td>
-                    <td>10/7/2021</td>
+                    <td>Vụ án</td>
+                    <td>Franz Kafka</td>
+                    <td>10/7/2025</td>
                 </tr>
                 <tr>
-                    <td>Tanishq</td>
-                    <td>Wings Of Fire</td>
-                    <td>15/9/2021</td>
+                    <td> J.D. Salinger</td>
+                    <td>Bắt trẻ đồng xanh</td>
+                    <td>15/9/2025 </td>
                 </tr>
                 <tr>
-                    <td>Akhil</td>
-                    <td>The Secret</td>
-                    <td>02/9/2021</td>
+                    <td>Nguyễn Nhật Ánh</td>
+                    <td>Ngày xưa có một chuyện tình</td>
+                    <td>02/9/2025 </td>
                 </tr>
                 <tr>
-                    <td>Surya</td>
-                    <td>Bad Guys</td>
-                    <td>21/7/2021</td>
+                    <td>Nguyễn Ngọc Tư</td>
+                    <td>Cánh đồng bất tận</td>
+                    <td>21/7/2025 </td>
                 </tr>
                 <tr>
-                    <td>Dinesh</td>
-                    <td>Giovanni Rovelli</td>
-                    <td>02/7/2021</td>
+                    <td>Nguyễn Nhật Ánh</td>
+                    <td>Hoa vàng trên cỏ xanh</td>
+                    <td>02/7/2025 </td>
+                </tr>
+                <tr>
+                    <td>Đoàn Giỏi</td>
+                    <td>Hành trình vào rừng Nam</td>
+                    <td>02/7/2025 </td>
+                </tr>
+                <tr>
+                    <td>Bảo Ninh</td>
+                    <td>Đêm chiến tranh dài</td>
+                    <td>02/7/2025 </td>
+                </tr>
+                <tr>
+                    <td>Phùng Quán</td>
+                    <td>Hồi ức tuổi thơ</td>
+                    <td>02/7/2025 </td>
                 </tr>
             </table>
         </div>
